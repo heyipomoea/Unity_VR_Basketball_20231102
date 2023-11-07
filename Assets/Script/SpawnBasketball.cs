4f3be44532cf9706ea3d936801290de3a51ce9f1
heyipomoea <heyipomoea@gmail.com>
2023-11-07 12:39:25 +0800

    Update 籃球生成

---
+++ Assets/Script/SpawnBasketball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Heyipomoea
+{
+    /// <summary>
+    /// 生成籃球
+    /// </summary>
+    public class SpawnBasketball : MonoBehaviour
+    {
+        [SerializeField, Header("籃球預製物")]
+        private GameObject prefabBasketball;
+
+        /// <summary>
+        /// 生成籃球按鈕
+        /// </summary>
+        private Button btnSpawnBasketball;
+
+        private void Awake()
+        {
+            btnSpawnBasketball = GameObject.Find("生成籃球按鈕").GetComponent<Button>();
+            btnSpawnBasketball.onClick.AddListener(Spwan);
+        }
+
+        private void Spwan()
+        {
+            Instantiate(prefabBasketball, transform.position, Quaternion.identity);
+        }
+    }
+}
+
