f5aba579e27afaf9f890f0c8293b7de46ab60c92
heyipomoea <heyipomoea@gmail.com>
2023-11-16 10:17:39 +0800

    Update 打包完成

---
+++ Assets/Script/SceneSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Heyipomoea
+{
+    /// <summary>
+    /// 場景系統
+    /// </summary>
+    public class SceneSystem : MonoBehaviour
+    {
+        private Button btnReplay, btnQuit;
+
+        private void Awake()
+        {
+            btnReplay = GameObject.Find("重新開始按鈕").GetComponent<Button>();
+            btnQuit = GameObject.Find("離開遊戲按鈕").GetComponent<Button>();
+
+            btnQuit.onClick.AddListener(() => Application.Quit());
+            btnReplay.onClick.AddListener(() =>
+            {
+                string currentScene = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentScene);
+            });
+        }
+    }
+}
+
